57bc117dc3d83cc8eb71b143cfc5b35905e8d0a0
Marcos <marcostulli@hotmail.com>
2025-07-21 19:21:05 +0200

    Change main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,7 @@ export default function HomePage() {
         sx={{ p: 3, bgcolor: "background.default", color: "secondary.main" }}
       >
         <Typography variant="h4" gutterBottom sx={{ color: "secondary.main" }}>
-          Welcome to the Home Page!
-        </Typography>
-        <Typography paragraph sx={{ color: "secondary.main" }}>
-          This is the main content area. It will adjust its width based on the
-          sidebar's state.
-        </Typography>
-        <Typography paragraph sx={{ color: "secondary.main" }}>
-          Use the arrow icon in the sidebar to toggle its visibility.
+          Welcome to Bee Bank app
         </Typography>
       </Box>
     </ProtectedPage>
